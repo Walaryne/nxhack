557de3db401c45fd4d64bf5af8f2674ffc37a4c5
Walaryne <hazardoustheradioactive@gmail.com>
2019-10-22 21:25:53 -0400

    Added some exception handling for slot function

--- src/teleport.cpp
+++ src/teleport.cpp
@@ -64,10 +64,10 @@ void saveloc(std::vector<std::string>* commandvec) {
 void slot(std::vector<std::string>* commandvec) {
 	try {
 		currentTeleportSlot = std::stoi(commandvec->at(1));
-	} catch(std::out_of_range& e) {
-		std::cout << "Argument would overflow an unsigned long! (Jeez step it down a bit ey?)\n";
 	} catch(std::invalid_argument& e) {
-		std::cout << "Argument isn't a number!\n";
+		std::cout << "Argument isn't a number.\n";
+	} catch(std::out_of_range& e) {
+		std::cout << "Argument is WAY too big.\n";
 	}
 }
 
